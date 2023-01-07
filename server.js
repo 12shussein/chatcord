95a979939bbaf09708b969bce56d4cdc34b026f3
Abdirahim Hussein <drahimhussein@gmail.com>
2023-01-07 14:24:13 +0000

    completed a few steps

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
+
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public folder')));
 
+//run when client connects
+io.on('connection', socket => {
+    console.log('new ws connection....');
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
